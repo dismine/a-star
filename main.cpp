0b9dfef2dd1c0f7c5c2dbbecfed987bb5f209ddc
Roman Telezhynskyi <dismine@gmail.com>
2016-06-03 17:16:12 +0300

    Managed to install GLUT library. For testing that all is ok added simple glut example.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,43 @@
 #include "stdafx.h"
+#include <GL\glut.h>
 
+void DisplayCB(void)		      /* function called whenever redisplay needed */
+{
+	glClear(GL_COLOR_BUFFER_BIT); /* clear the display */
+	glColor3f(1.0, 1.0, 1.0);	  /* set current color to white */
+	glBegin(GL_POLYGON);		  /* draw filled triangle */
+	glVertex2i(200,125);		  /* specify each vertex of triangle */
+	glVertex2i(100,375);
+	glVertex2i(300,375);
+	glEnd();				      /* OpenGL draws the filled triangle */
+	glFlush();				      /* Complete any pending operations */
+}
+
+void KeyCB(unsigned char key, int x, int y)	/* called on key press */
+{
+	if( key == 'q' ) 
+	{
+		exit(0);
+	}
+}
 
 int main(int argc, char **argv)
 {
+	glutInit(&argc, argv);		                  /* initialize GLUT system */
+	glutInitDisplayMode(GLUT_RGB);
+	glutInitWindowSize(400, 500);		          /* width=400pixels height=500pixels */
+	glutCreateWindow("Test");	                  /* create window */
+	glMatrixMode(GL_PROJECTION); 
+	glLoadIdentity(); 
+	glClearColor(0.0, 0.0, 0.0, 0.0);	          /* set background to black */
+	gluOrtho2D(0, 400, 0, 500);		              /* how object is mapped to window */
+	glutDisplayFunc(DisplayCB);		              /* set window's display callback */
+	glutKeyboardFunc(KeyCB);		              /* set window's key callback */
+
+	glutMainLoop();			                      /* start processing events... */
+
+	/* execution never reaches this point */
+
 	return 0;
 }
 
